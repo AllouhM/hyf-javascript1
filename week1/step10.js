f44c5da10e4b69a2bf8c8c3ca16c64851523a2a6
AllouhM <mahmoud.allouh0@gmail.com>
2018-04-28 22:50:59 +0200

    amending code to work properly

--- week1/step10.js
+++ week1/step10.js
@@ -8,13 +8,10 @@ arr.push(y);
 console.log(arr);
 console.log(typeof x, typeof y);
 console.log(typeof arr);
-<<<<<<< HEAD
+
 if (typeof x !== typeof y) {
     console.log('We can combin different datat type in an array');
-=======
-if (typeof x != typeof y) {
-    console.log('We can combine different data type in an array');
->>>>>>> 83d9331b7ede70a2b1125c9cf7c49389a22d317a
+
 }
 
 let option1 = 6 / 0;
@@ -27,9 +24,3 @@ if (option1 === option2) {
 else {
     console.log('Sorry,, infinities can not be compared');
 }
-<<<<<<< HEAD
-=======
-else{
- console.log('Sorry, infinities can not be compared');
-}
->>>>>>> 83d9331b7ede70a2b1125c9cf7c49389a22d317a
--- week1/step3.js
+++ week1/step3.js
@@ -2,10 +2,6 @@
 let x
 console.log('the value of my variable x will be: undefined');
 console.log(x);
-<<<<<<< HEAD
 x = 6;
-=======
- x = 6;
->>>>>>> 83d9331b7ede70a2b1125c9cf7c49389a22d317a
 console.log('the value of my variable x will be:6');
 console.log(x);
